e387889095d176ce7e4e0ac767986942179ccb00
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-06-29 15:37:07 +0100

    --add register on backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -6,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         {/* Other routes */}
       </Routes>
     </Router>
