287256d55dae2ea7828c0cda95b77cd7cf56bfc7
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-07-03 18:46:07 +0100

    --add home routing and start ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,29 @@
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-wrap justify-center">
-        <p>This is home</p>
-      </div>
-    </div>
+    <>
+      <header className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="logo">
+            <img src="../assets/logo-dark.svg" alt="Logo" className="h-8" />
+          </div>
+          <div className="flex space-x-4">
+            <a href="#" className="hover:underline">
+              Option 1
+            </a>
+            <a href="#" className="hover:underline">
+              Option 2
+            </a>
+            <a href="#" className="hover:underline">
+              Option 3
+            </a>
+          </div>
+        </div>
+        <div className="container mx-auto">
+          <div className="flex flex-wrap justify-center">
+            <p>This is home</p>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
