747424b489fbd19d5aaae25c92d915db17b8a24c
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-07-07 15:07:50 +0100

    --add toggle theme btn to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../index.css";
 
 function Sidebar() {
@@ -31,21 +31,24 @@ function Sidebar() {
         <a href="#" className="text-custom-dark dark:text-custom-light">
           Link 3
         </a>
+        {/* Buttons moved inside the sidebar */}
+        <div>
+          <button
+            className={`bg-custom-blue mt-4${
+              isDarkMode ? "bg-custom-light" : "bg-custom-dark"
+            } text-custom-white rounded-full p-3 pl-6 pr-6 theme-btn`}
+            onClick={toggleDarkMode}
+          >
+            Toggle Dark Mode
+          </button>
+          <button
+            className="bg-custom-blue text-custom-white rounded-full p-3 pl-6 pr-6 toggle-btn"
+            onClick={toggleSidebar}
+          >
+            {isOpen ? "Close" : "Open"} Sidebar
+          </button>
+        </div>
       </div>
-      <button
-        className="bg-custom-blue text-custom-white rounded-full p-3 pl-6 pr-6 toggle-btn"
-        onClick={toggleSidebar}
-      >
-        {isOpen ? "Close" : "Open"} Sidebar
-      </button>
-      <button
-        className={`mt-4 ${
-          isDarkMode ? "bg-custom-light" : "bg-custom-dark"
-        } text-custom-white rounded-full p-3 pl-6 pr-6`}
-        onClick={toggleDarkMode}
-      >
-        Toggle Dark Mode
-      </button>
     </>
   );
 }
