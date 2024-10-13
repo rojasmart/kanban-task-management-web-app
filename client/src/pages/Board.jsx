ddaeadac7559307f6b05e7831f86d7f3eeb79aa0
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2024-10-13 14:37:45 +0100

    --add close btn to modal create column

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -78,7 +78,7 @@ const Board = ({ board }) => {
       </div>
       {isModalOpen && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-custom-white dark:bg-gray-800 rounded-lg shadow-lg w-full max-w-lg p-6">
+          <div className="relative bg-custom-white dark:bg-gray-800 rounded-lg shadow-lg w-full max-w-lg p-6">
             <span
               className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 cursor-pointer"
               onClick={toggleModal}
